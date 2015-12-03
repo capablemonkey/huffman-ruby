3e2a3afb7bdebc35c29158ad87f546b2fa62d82c
Gordon Zheng <gordon@greenhouse.io>
2015-12-02 21:08:39 -0500

    return a sorted freq list

--- main.rb
+++ main.rb
@@ -1,15 +1,18 @@
 require 'pp'
+require 'profile'
 
 inputFile = File.open('sample.txt', 'r')
 
 def getFrequencies(inputFile)
   frequencies = {}
 
+  # TODO: pre-populate frequencies with 0 to skip .nil? check.
+
   inputFile.each_byte do |k|
     frequencies[k] = if frequencies[k].nil? then 0 else (frequencies[k] + 1) end
   end
 
-  frequencies
+  frequencies.to_a.sort_by! { |k, v| v } .reverse
 end
 
 pp getFrequencies(inputFile)
